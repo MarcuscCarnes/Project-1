166e81176c1209d869339aa6bc74b9fdf204b293
AlexMallett <mallett.g.alex@gmail.com>
2021-04-01 23:44:36 -0500

    Updated Links

--- Scripts/restaurants.js
+++ Scripts/restaurants.js
@@ -1,11 +1,11 @@
 var i;
 for(i = 0; i < Document.getElementsByClassName('restaurant').length; i++) {
     Document.getElementsByClassName('restaurant')[i].addEventListener('click', myFunction);
-}
+};
 
 function myFunction() {
     alert ("Hello World!")
-}
+};
 
 Document.getElementsByClassName('restaurant')[0].addEventListener('click', myFunction);
 
