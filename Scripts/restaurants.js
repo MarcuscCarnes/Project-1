a1a2f31afdec8bb7df9ac635e1928b8bf6854653
AlexMallett <mallett.g.alex@gmail.com>
2021-04-06 16:06:29 -0500

    Finished Working On It

--- Scripts/restaurants.js
+++ Scripts/restaurants.js
@@ -1,12 +1,69 @@
-var i;
-for(i = 0; i < Document.getElementsByClassName('restaurant').length; i++) {
-    Document.getElementsByClassName('restaurant')[i].addEventListener('click', myFunction);
-};
+var infoOpen = false;
 
-function myFunction() {
-    alert ("Hello World!")
-};
+function closeInfo(x) {
+    var y = document.getElementById(x);
+    y.style.display = "none";
+}
 
-Document.getElementsByClassName('restaurant')[0].addEventListener('click', myFunction);
+function closeAllInfo() {
+    closeInfo("r1Info");
+    closeInfo("r2Info");
+    closeInfo("r3Info");
+    closeInfo("r4Info");
+}
 
-Document.addEventListener('click', myFunction);
+function r1() {
+    var x = document.getElementById("r1Info");
+    if (x.style.display === "none") {
+        if(infoOpen == true) {
+            closeAllInfo();
+        }
+        x.style.display = "block";
+        infoOpen = true;
+    } else {
+        x.style.display = "none";
+        infoOpen = false;
+    }
+}
+
+function r2() {
+    var x = document.getElementById("r2Info");
+    if (x.style.display === "none") {
+        if(infoOpen == true) {
+            closeAllInfo();
+        }
+        x.style.display = "block";
+        infoOpen = true;
+    } else {
+        x.style.display = "none";
+        infoOpen = false;
+    }
+}
+
+function r3() {
+    var x = document.getElementById("r3Info");
+    if (x.style.display === "none") {
+        if(infoOpen == true) {
+            closeAllInfo();
+        }
+        x.style.display = "block";
+        infoOpen = true;
+    } else {
+        x.style.display = "none";
+        infoOpen = false;
+    }
+}
+
+function r4() {
+    var x = document.getElementById("r4Info");
+    if (x.style.display === "none") {
+        if(infoOpen == true) {
+            closeAllInfo();
+        }
+        x.style.display = "block";
+        infoOpen = true;
+    } else {
+        x.style.display = "none";
+        infoOpen = false;
+    }
+}
